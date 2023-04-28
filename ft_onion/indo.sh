56fc00f022f7fab7b77212fd1e7796fadb99d043
evil-A <57819409+evil-A@users.noreply.github.com>
2023-04-28 16:39:36 +0200

    Add files via upload

--- ft_onion/indo.sh
+++ ft_onion/indo.sh
@@ -17,6 +17,18 @@ blue=$'\033[0;34m'
 cyan=$'\033[1;96m'
 reset=$'\033[0;39m'
 
+
+echo "${cyan}Docker${blue} is cleaning the system of containers!${reset}"
+docker system prune -af
+
+echo "${cyan}Docker${blue} creating the onion container${reset}"
+
 docker build -t onion .
+
+echo "${cyan}Docker${blue} showing images${reset}"
+
 docker images
-docker run -it -p 80:80 -p 4242:4242 onion
+
+echo "${cyan}Docker${blue} running onion container${reset}"
+
+docker run -it --name onion -p 80:80 -p 4242:4242 onion
